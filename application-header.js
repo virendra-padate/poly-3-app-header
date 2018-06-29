edaa94c8688129dc0c7f7afb7468b79386696773
Virendra Padate <virendra.padate@tcs.com>
2018-06-29 17:51:48 +0530

    'na'

---
+++ application-header.js
@@ -0,0 +1,38 @@
+import {
+  PolymerElement,
+  html
+} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `application-header`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class ApplicationHeader extends PolymerElement {
+  static get template() {
+    return html `
+      <style>
+        :host {
+          display: block;
+          background: #4285f4;
+          color: white;
+          padding: 0.5em 1em;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'application-header-1',
+      },
+    };
+  }
+}
+
+window.customElements.define('application-header', ApplicationHeader);
